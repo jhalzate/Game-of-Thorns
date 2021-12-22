78961ef3b8b540ea53624230ea602a0dce9c7818
jhalzate <68299690+jhalzate@users.noreply.github.com>
2021-12-22 15:48:33 -0600

    Update ex45c.py

--- ex45c.py
+++ ex45c.py
@@ -1,9 +1,4 @@
-# from sys import exit
-# import time
-# import sys
-# from random import randint
-from ex45a import * # Engine
-# from ex45b import *
+from ex45a import *
 from ex45nice import *
 
 a_map = Map('central_kingdom')
